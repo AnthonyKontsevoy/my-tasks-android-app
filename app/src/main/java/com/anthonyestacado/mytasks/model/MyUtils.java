2744ef34af007a0f94767d91c32584a16b0ec1fa
anthonykontsevoy <anton.96k@gmail.com>
2018-03-14 16:08:11 +0200

    Fixed the fragment that displays user's tasks in a list of cards.

--- app/src/main/java/com/anthonyestacado/mytasks/model/MyUtils.java
+++ app/src/main/java/com/anthonyestacado/mytasks/model/MyUtils.java
@@ -12,12 +12,64 @@ public class MyUtils {
 
     public static String selectOnlyDateFromString(String inputString) {
         StringBuilder stringBuilder = new StringBuilder(inputString);
-        return stringBuilder.substring(0, 9);
+        String month = stringBuilder.substring(5, 7);
+        switch (month) {
+            case "01": {
+                month = "Jan";
+                break;
+            }
+            case "02": {
+                month = "Feb";
+                break;
+            }
+            case "03": {
+                month = "Mar";
+                break;
+            }
+            case "04": {
+                month = "Apr";
+                break;
+            }
+            case "05": {
+                month = "May";
+                break;
+            }
+            case "06": {
+                month = "Jun";
+                break;
+            }
+            case "07": {
+                month = "Jul";
+                break;
+            }
+            case "08": {
+                month = "Aug";
+                break;
+            }
+            case "09": {
+                month = "Sep";
+                break;
+            }
+            case "10": {
+                month = "Oct";
+                break;
+            }
+            case "11": {
+                month = "Nov";
+                break;
+            }
+            case "12": {
+                month = "Dec";
+                break;
+            }
+        }
+        String convertedDate = month + " " + stringBuilder.substring(8, 10);
+        return convertedDate;
     }
 
     public static String selectOnlyTimeFromString(String inputString) {
         StringBuilder stringBuilder = new StringBuilder(inputString);
-        return stringBuilder.substring(11,15);
+        return stringBuilder.substring(11,16);
     }
 
     public static List<UserTask> getListOfUserTasksForDebug() {
